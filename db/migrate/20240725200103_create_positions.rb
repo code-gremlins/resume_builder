9db2468bc979ba7242da97996f133075c9e05ac6
jasonguyperson <jasonshane87@gmail.com>
2024-07-25 14:06:30 -0600

    Create positions table

---
+++ db/migrate/20240725200103_create_positions.rb
@@ -0,0 +1,14 @@
+class CreatePositions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :positions do |t|
+      t.string     :company_name
+      t.string     :title
+      t.text       :description
+      t.date       :start_date
+      t.date       :end_date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
